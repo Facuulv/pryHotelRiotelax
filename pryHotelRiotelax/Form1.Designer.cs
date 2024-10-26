5d9222e757f47b065c53e3ab40601d22ce2044c2
Facundo Lopez Vital <facu_lv_13@hotmail.com>
2024-10-26 19:06:17 -0300

    Agregar archivos de proyecto.

---
+++ pryHotelRiotelax/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace pryHotelRiotelax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkSpa = new System.Windows.Forms.CheckBox();
+            this.chkInternet = new System.Windows.Forms.CheckBox();
+            this.chkBar = new System.Windows.Forms.CheckBox();
+            this.chkGimnasio = new System.Windows.Forms.CheckBox();
+            this.cmbPiso = new System.Windows.Forms.ComboBox();
+            this.cmbHabitacion = new System.Windows.Forms.ComboBox();
+            this.numDias = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblServicios = new System.Windows.Forms.Label();
+            this.lblTotalDia = new System.Windows.Forms.Label();
+            this.lblPrecioHabitacion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numDias)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkSpa);
+            this.groupBox1.Controls.Add(this.chkInternet);
+            this.groupBox1.Controls.Add(this.chkBar);
+            this.groupBox1.Controls.Add(this.chkGimnasio);
+            this.groupBox1.Location = new System.Drawing.Point(235, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Servicios Adicionales";
+            // 
+            // chkSpa
+            // 
+            this.chkSpa.AutoSize = true;
+            this.chkSpa.Location = new System.Drawing.Point(7, 100);
+            this.chkSpa.Name = "chkSpa";
+            this.chkSpa.Size = new System.Drawing.Size(54, 20);
+            this.chkSpa.TabIndex = 3;
+            this.chkSpa.Text = "Spa";
+            this.chkSpa.UseVisualStyleBackColor = true;
+            this.chkSpa.CheckedChanged += new System.EventHandler(this.chkSpa_CheckedChanged);
+            // 
+            // chkInternet
+            // 
+            this.chkInternet.AutoSize = true;
+            this.chkInternet.Location = new System.Drawing.Point(7, 74);
+            this.chkInternet.Name = "chkInternet";
+            this.chkInternet.Size = new System.Drawing.Size(139, 20);
+            this.chkInternet.TabIndex = 2;
+            this.chkInternet.Text = "Wifi de 100 Megas";
+            this.chkInternet.UseVisualStyleBackColor = true;
+            this.chkInternet.CheckedChanged += new System.EventHandler(this.chkInternet_CheckedChanged);
+            // 
+            // chkBar
+            // 
+            this.chkBar.AutoSize = true;
+            this.chkBar.Location = new System.Drawing.Point(7, 48);
+            this.chkBar.Name = "chkBar";
+            this.chkBar.Size = new System.Drawing.Size(104, 20);
+            this.chkBar.TabIndex = 1;
+            this.chkBar.Text = "Bar del hotel";
+            this.chkBar.UseVisualStyleBackColor = true;
+            this.chkBar.CheckedChanged += new System.EventHandler(this.chkBar_CheckedChanged);
+            // 
+            // chkGimnasio
+            // 
+            this.chkGimnasio.AutoSize = true;
+            this.chkGimnasio.Location = new System.Drawing.Point(7, 22);
+            this.chkGimnasio.Name = "chkGimnasio";
+            this.chkGimnasio.Size = new System.Drawing.Size(86, 20);
+            this.chkGimnasio.TabIndex = 0;
+            this.chkGimnasio.Text = "Gimnasio";
+            this.chkGimnasio.UseVisualStyleBackColor = true;
+            this.chkGimnasio.CheckedChanged += new System.EventHandler(this.chkGimnasio_CheckedChanged);
+            // 
+            // cmbPiso
+            // 
+            this.cmbPiso.FormattingEnabled = true;
+            this.cmbPiso.Location = new System.Drawing.Point(27, 53);
+            this.cmbPiso.Name = "cmbPiso";
+            this.cmbPiso.Size = new System.Drawing.Size(149, 24);
+            this.cmbPiso.TabIndex = 1;
+            this.cmbPiso.SelectedIndexChanged += new System.EventHandler(this.cmbPiso_SelectedIndexChanged);
+            // 
+            // cmbHabitacion
+            // 
+            this.cmbHabitacion.FormattingEnabled = true;
+            this.cmbHabitacion.Location = new System.Drawing.Point(27, 123);
+            this.cmbHabitacion.Name = "cmbHabitacion";
+            this.cmbHabitacion.Size = new System.Drawing.Size(149, 24);
+            this.cmbHabitacion.TabIndex = 2;
+            // 
+            // numDias
+            // 
+            this.numDias.Location = new System.Drawing.Point(235, 53);
+            this.numDias.Name = "numDias";
+            this.numDias.Size = new System.Drawing.Size(144, 22);
+            this.numDias.TabIndex = 3;
+            this.numDias.ValueChanged += new System.EventHandler(this.numDias_ValueChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblResultado);
+            this.groupBox2.Controls.Add(this.lblServicios);
+            this.groupBox2.Controls.Add(this.lblTotalDia);
+            this.groupBox2.Controls.Add(this.lblPrecioHabitacion);
+            this.groupBox2.Location = new System.Drawing.Point(21, 249);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(275, 139);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resumen";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(6, 107);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 16);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // lblServicios
+            // 
+            this.lblServicios.AutoSize = true;
+            this.lblServicios.Location = new System.Drawing.Point(6, 81);
+            this.lblServicios.Name = "lblServicios";
+            this.lblServicios.Size = new System.Drawing.Size(0, 16);
+            this.lblServicios.TabIndex = 10;
+            // 
+            // lblTotalDia
+            // 
+            this.lblTotalDia.AutoSize = true;
+            this.lblTotalDia.Location = new System.Drawing.Point(6, 55);
+            this.lblTotalDia.Name = "lblTotalDia";
+            this.lblTotalDia.Size = new System.Drawing.Size(0, 16);
+            this.lblTotalDia.TabIndex = 9;
+            // 
+            // lblPrecioHabitacion
+            // 
+            this.lblPrecioHabitacion.AutoSize = true;
+            this.lblPrecioHabitacion.Location = new System.Drawing.Point(6, 28);
+            this.lblPrecioHabitacion.Name = "lblPrecioHabitacion";
+            this.lblPrecioHabitacion.Size = new System.Drawing.Size(0, 16);
+            this.lblPrecioHabitacion.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Seleccionar Piso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Seleccione Habitacion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cantidad de dias";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(55, 176);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(95, 43);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(313, 264);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(95, 43);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(313, 332);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(95, 43);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(442, 431);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.numDias);
+            this.Controls.Add(this.cmbHabitacion);
+            this.Controls.Add(this.cmbPiso);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reserva Hotel Riotelax";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numDias)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbPiso;
+        private System.Windows.Forms.ComboBox cmbHabitacion;
+        private System.Windows.Forms.NumericUpDown numDias;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblPrecioHabitacion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkSpa;
+        private System.Windows.Forms.CheckBox chkInternet;
+        private System.Windows.Forms.CheckBox chkBar;
+        private System.Windows.Forms.CheckBox chkGimnasio;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblServicios;
+        private System.Windows.Forms.Label lblTotalDia;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ pryHotelRiotelax/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryHotelRiotelax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Funcion con los valores inciales
+        public void ValoresIniciales()
+        {
+            cmbPiso.Items.Clear();
+            cmbHabitacion.Items.Clear();
+            cmbPiso.Items.Add("Planta Baja");
+            cmbPiso.Items.Add("1° Piso");
+            cmbPiso.Items.Add("2° Piso");
+            cmbPiso.Items.Add("3° Piso");
+            cmbPiso.Items.Add("4° Piso");
+            cmbPiso.Items.Add("5° Piso (Suit)");
+            cmbPiso.Items.Add("6° Piso (Suit)");
+            numDias.Maximum = 30;
+
+            numDias.Value = 0;
+            cmbPiso.Text = "";
+            cmbHabitacion.Text = ""; 
+            chkBar.Checked = false;
+            chkGimnasio.Checked = false;
+            chkInternet.Checked = false;
+            chkSpa.Checked = false;
+
+            lblPrecioHabitacion.Text = "";
+            lblTotalDia.Text = "";
+            lblServicios.Text = "";
+            lblResultado.Text = "";
+
+            total = 0;
+            precioPorHabitacion = 0;
+            valorDia = 0;
+            extras = 0;
+        }
+
+        //Variables globales
+        int total = 0;
+        int precioPorHabitacion = 0;
+        int valorDia = 0;
+        int extras = 0;
+
+        // Funcion para calcular y mostrar los resultados
+        private void CalculodeReserva()
+        {
+            lblServicios.Text = "Servicios: $" + extras.ToString();
+            lblTotalDia.Text = "El total por los dias ingresados es: $" + valorDia.ToString();
+            lblPrecioHabitacion.Text = "El precio por habitacion por día es: $" + precioPorHabitacion.ToString();
+            total = valorDia + extras;
+            lblResultado.Text = "El monto final es: $" + total.ToString();
+        }
+
+        /*Boton Calcular, controlo que dias y habitacion sean distintos de nada y 0. 
+          Si no se coloca un valor, MessageBox con el error en ambos casos. */
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (cmbHabitacion.Text != "")
+            {
+                int diasMayor0 = (int)numDias.Value;
+                if (diasMayor0 != 0)
+                {
+                    CalculodeReserva();
+                }
+                else
+                {
+                    MessageBox.Show("Error, la cantidad de dias para reservar no puede ser 0.", "¡Advertencia!");
+                }
+            } else
+            {
+                MessageBox.Show("Debe colocar un piso y una habitación.", "¡Advertencia!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ValoresIniciales();
+        }
+
+        /* Segun que piso selecciono me coloca la cant de habitaciones con un while. Ademas para los
+           indices 0 a 4 se agrega "Habitacion" y para el resto "Suit". */
+        private void cmbPiso_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int desde = 1;
+            int hasta;
+            cmbHabitacion.Items.Clear();
+            cmbHabitacion.Text = "";
+            switch (cmbPiso.SelectedIndex)
+            {
+                case 0:
+                    hasta = 10;                  
+                    break;
+                case 1:
+                    desde = 101;
+                    hasta = 110;      
+                    break;
+                case 2:
+                    desde = 201;
+                    hasta = 210;
+                    break;
+                case 3:
+                    desde = 301;
+                    hasta = 310;
+                    break;
+                case 4:
+                    desde = 401;
+                    hasta = 410;
+                    break;
+                case 5:
+                    desde = 501;
+                    hasta = 502;
+                    break;
+                default:
+                    desde = 601;
+                    hasta = 602;
+                    break;
+            }
+            while (desde <= hasta)
+            {
+                if(cmbPiso.SelectedIndex <= 4)
+                {
+                    cmbHabitacion.Items.Add("Habitacion " + desde);
+                } else
+                {
+                    cmbHabitacion.Items.Add("Suit " + desde);
+                }
+                desde++;
+            }
+        }
+
+        /* valorDia cambia segun el index de cmbPiso. Si es <= 4 el valorDia es la cant de dias * 60.
+           Si es otro caso, es * 130. Agregue una variable para guardar el precio por habitacion. */
+        private void numDias_ValueChanged(object sender, EventArgs e)
+        {
+            int dia = (int)numDias.Value;
+            if (dia > 0 && dia <= 30)
+            {
+                if (cmbPiso.SelectedIndex <= 4)
+                {
+                    precioPorHabitacion = 60;
+                    valorDia = dia * 60;
+                }
+                else
+                {
+                    precioPorHabitacion = 130;
+                    valorDia = dia * 130;
+                }
+            }
+        }
+
+        /*Funcion para sumar o restar segun si el checkBox esta seleccionado o no. Si esta seleccionado,
+          osea true, se suma 50 sino se resta. Los parametros son cada checkBox. */
+        public void Adicionales(bool valor)
+        {
+            if (valor)
+            {
+                extras += 50;
+            }
+            else
+            {
+                extras -= 50;
+            }
+        }
+        private void chkGimnasio_CheckedChanged(object sender, EventArgs e)
+        {
+            Adicionales(chkGimnasio.Checked);
+        }
+        private void chkBar_CheckedChanged(object sender, EventArgs e)
+        {
+            Adicionales(chkBar.Checked);
+        }
+        private void chkInternet_CheckedChanged(object sender, EventArgs e)
+        {
+            Adicionales(chkInternet.Checked);
+        }
+        private void chkSpa_CheckedChanged(object sender, EventArgs e)
+        {
+            Adicionales(chkSpa.Checked);
+        }
+
+        //Boton para salir del programa
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Boton para limpiar los componentes
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            ValoresIniciales();
+        }
+    }
+}
